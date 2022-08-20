a85587f270b565f590d12a7c9a683b54e09fc1f7
Sessyoinpornhob <71918373+Sessyoinpornhob@users.noreply.github.com>
2022-08-20 17:12:45 +0800

    增删物体
    -增删物体

--- Assets/Scripts/cursorMovement.cs
+++ Assets/Scripts/cursorMovement.cs
@@ -23,7 +23,7 @@ public class cursorMovement : MonoBehaviour
         if (Physics.Raycast(ray, out hit) && hit.transform.CompareTag("gridElement"))
         {
             this.transform.position = hit.collider.transform.position;
-            //Debug.Log(hit.collider.name);
+            
             lastHit = hit.collider.gameObject.GetComponent<gridElement>();
 
             if (Input.GetMouseButtonDown(1))
@@ -35,30 +35,60 @@ public class cursorMovement : MonoBehaviour
 
     public void SetCursorButton(int input)
     {
+        coord coord = lastHit.GetCoord();
+        int width = levelGenerator.instance.width;
+        int height = levelGenerator.instance.height;
+        
         switch (input)
         {
             case 0:
                 //remove gridElement
-                //Debug.Log("666");
-                lastHit.SetDisable();
+                if (coord.y > 0)
+                {
+                    lastHit.SetDisable();
+                }
                 break;
             case 1:
                 // add x+
+                if (coord.x < width - 1)
+                { 
+                    levelGenerator.instance.gridElements[coord.x + width * (coord.z + width * coord.y ) + 1].SetEnable();
+                }
                 break;
             case 2:
                 // add x-
+                if (coord.x > 0)
+                { 
+                    levelGenerator.instance.gridElements[coord.x + width * (coord.z + width * coord.y ) - 1].SetEnable();
+                }
                 break;
             case 3:
                 // add z+
+                if (coord.z < width - 1)
+                { 
+                    levelGenerator.instance.gridElements[coord.x + width * (coord.z + 1 + width * coord.y )].SetEnable();
+                }
                 break;
             case 4:
                 // add z-
+                if (coord.z > 0)
+                { 
+                    levelGenerator.instance.gridElements[coord.x + width * (coord.z -1 + width * coord.y )].SetEnable();
+                }
                 break;
             case 5:
                 // add y+
+                if (coord.y < height - 1)
+                { 
+                    levelGenerator.instance.gridElements[coord.x + width * (coord.z + width * (coord.y + 1))].SetEnable();
+                }
                 break;
             case 6:
                 // add y-
+                if (coord.y > 0)
+                { 
+                    levelGenerator.instance.gridElements[coord.x + width * (coord.z + width * (coord.y - 1))].SetEnable();
+                }
                 break;
         }
     }
